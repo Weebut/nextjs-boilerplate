3fc9470ffc9500785b1e51338fe55254310c1155
Seunghyuk <seunghyukoh0@gmail.com>
2022-05-24 16:32:17 +0900

    Update root file to provide cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import store from '@redux/store';
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import { CookiesProvider } from 'react-cookie';
 import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Provider store={store}>
-        <Component {...pageProps} />
+        <CookiesProvider>
+          <Component {...pageProps} />
+        </CookiesProvider>
       </Provider>
     </UserProvider>
   );

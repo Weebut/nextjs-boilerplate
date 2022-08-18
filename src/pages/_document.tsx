a0d6d5700f446ddb19d30665d98fd2b7bf269619
ohshyuk5 <seunghyukoh0@gmail.com>
2022-08-18 11:47:55 +0900

    Add mui packages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
         </Head>
         <body>
           <Main />

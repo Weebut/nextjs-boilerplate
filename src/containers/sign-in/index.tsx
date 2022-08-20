7023da18b43dae791e05f9b270501c31fcede23e
ohshyuk5 <seunghyukoh0@gmail.com>
2022-08-20 21:51:21 +0900

    Update sign in container

--- src/containers/sign-in/index.tsx
+++ src/containers/sign-in/index.tsx
@@ -1,6 +1,7 @@
 import { Layout } from '@components/layouts/layout.component';
 import { Strip } from '@components/strips/strip.component';
 import { AuthProviders } from '@libs/types/auth-providers';
+import { Button, TextField } from '@mui/material';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
@@ -18,17 +19,34 @@ export function SignInContainer({ providers }: SignInContainerProps) {
   return (
     <Layout session={null}>
       <Strip>
-        <div className="flex flex-col">
-          {Object.values(providers).map((provider) => (
-            <div
-              key={provider.name}
-              className="rounded border p-4 hover:bg-gray-300"
-            >
-              <button onClick={() => signIn(provider.id, { callbackUrl })}>
+        <div className="flex flex-col items-center space-y-12 py-32">
+          <div className="">
+            <span className="text-2xl font-bold">로그인</span>
+          </div>
+          <form className="flex w-full max-w-[300px] flex-col space-y-6">
+            <TextField id="outlined-basic" label="아이디" variant="outlined" />
+            <TextField
+              id="outlined-basic"
+              label="비밀번호"
+              variant="outlined"
+              type="password"
+            />
+            <Button variant="contained" className="py-2 text-lg">
+              로그인
+            </Button>
+          </form>
+          <div className="flex w-full max-w-[300px] flex-col space-y-6">
+            {Object.values(providers).map((provider) => (
+              <Button
+                key={provider.name}
+                variant="contained"
+                onClick={() => signIn(provider.id, { callbackUrl })}
+                className="p-4"
+              >
                 Sign in with {provider.name}
-              </button>
-            </div>
-          ))}
+              </Button>
+            ))}
+          </div>
         </div>
       </Strip>
     </Layout>

8549733560a135512b21ffd260c7fa038fe307d8
JaeminBest <mikjm98@kaist.ac.kr>
2022-08-25 17:06:04 +0900

    hotfix: local development env without emulator

--- src/libs/firebase/client.ts
+++ src/libs/firebase/client.ts
@@ -1,22 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { production } from '@libs/constants/node';
 import { initializeApp } from 'firebase/app';
-import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
-const firebaseConfig =
-  process.env.NODE_ENV === production
-    ? {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      }
-    : {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      };
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 // Initialize Firebase
 const firebaseApp = initializeApp(
@@ -25,9 +18,3 @@ const firebaseApp = initializeApp(
 );
 export const firebaseAuth = getAuth(firebaseApp);
 firebaseAuth.languageCode = 'kr';
-
-if (process.env.NODE_ENV === production) {
-  // const firebaseAnalytics = getAnalytics(firebaseApp);
-} else {
-  connectAuthEmulator(firebaseAuth, 'http://localhost:9099');
-}

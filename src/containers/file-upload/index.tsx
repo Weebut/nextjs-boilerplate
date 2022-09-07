9481d80ed23d35a7712f3672a5200c7e6b1b789c
ohshyuk5 <seunghyukoh0@gmail.com>
2022-09-07 16:32:48 +0900

    Add file upload page

---
+++ src/containers/file-upload/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@components/layouts/layout.component';
+import { Strip } from '@components/strips/strip.component';
+
+export function FileUploadContainer() {
+  return (
+    <Layout>
+      <Strip>File upload</Strip>
+    </Layout>
+  );
+}

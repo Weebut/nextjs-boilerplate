63355c3358f0c691ea120e48ce4cf0d4c40c53c2
ohshyuk5 <seunghyukoh0@gmail.com>
2022-09-07 17:51:45 +0900

    Add api route for file upload

--- src/pages/api/file-upload.ts
+++ src/pages/api/file-upload.ts
@@ -1,5 +1,21 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { multerUploadAtLocal } from '@libs/multer/local';
+import { NextApiRequestWithFile } from '@libs/multer/type';
+import { NextApiResponse } from 'next';
+import nc from 'next-connect';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const uploadFile = multerUploadAtLocal.single('file');
+
+const handler = nc();
+handler.use(uploadFile);
+// Handle POST request
+handler.post<NextApiRequestWithFile, NextApiResponse>(async (req, res) => {
   res.status(201).end();
-}
+});
+
+export default handler;

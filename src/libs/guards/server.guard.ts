1a4e16f5202af625ccd2a26ff51fdaa1e2f7dd2c
Seunghyuk <seunghyukoh0@gmail.com>
2022-10-04 13:52:23 +0900

    Fix auth guards (#18)

--- src/libs/guards/server.guard.ts
+++ src/libs/guards/server.guard.ts
@@ -1,4 +1,5 @@
 import { GetServerSidePropsContext, Redirect } from 'next';
+import { stringifyUrl } from 'query-string';
 
 interface GuardResponse {
   props?: any;
@@ -7,12 +8,18 @@ interface GuardResponse {
 
 export function ServerSideAuthGuard({
   req,
+  resolvedUrl,
 }: GetServerSidePropsContext): GuardResponse {
   const token = req.session.idToken;
 
   if (!token) {
+    const destination = stringifyUrl({
+      url: '/sign-in',
+      query: { redirectTo: encodeURIComponent(resolvedUrl) },
+    });
+
     return {
-      redirect: { destination: '/sign-in', permanent: false },
+      redirect: { destination, permanent: false },
     };
   } else {
     return { props: { token } };

80f119d5cda699416fc28392caf9f42bcb5dd119
ohshyuk5 <seunghyukoh0@gmail.com>
2022-10-05 11:44:14 +0900

    Update app bar

--- apps/web/src/components/blocks/app-bars/default/index.tsx
+++ apps/web/src/components/blocks/app-bars/default/index.tsx
@@ -1,8 +1,10 @@
+import AccountButton from '@components/atoms/buttons/account';
+import NotificationButton from '@components/atoms/buttons/notification';
 import { useFirebaseUser } from '@hooks/firebase-user';
 import { LayoutConfig } from '@libs/config';
 import { MaxWidth } from '@libs/material-ui/types';
 import { AppBar as MaterialAppBar, Box, Container } from '@mui/material';
-import { Link } from 'shared-components';
+import { Link, LinkButton } from 'shared-components';
 
 const appBarHeight = LayoutConfig.appBar.height;
 const defaultMaxWidth = LayoutConfig.maxWidth as MaxWidth;
@@ -15,6 +17,7 @@ export default function AppBar({ maxWidth = defaultMaxWidth }: AppBarProps) {
   const { user, isLoading } = useFirebaseUser();
 
   const isLoggedIn = !!user;
+  const hasNewNotifications = true;
 
   return (
     <MaterialAppBar position="sticky" elevation={0} color="inherit">
@@ -37,17 +40,26 @@ export default function AppBar({ maxWidth = defaultMaxWidth }: AppBarProps) {
 
         <Box sx={{ flexGrow: 1 }} />
 
-        <Box sx={{ flexGrow: 0 }}>
-          {isLoading ? null : isLoggedIn ? (
-            <Link href="/my-page" fontWeight="bold" underline="none">
-              {user.email}
-            </Link>
-          ) : (
-            <Link href="/sign-in" fontWeight="bold" underline="none">
-              SIGN IN
-            </Link>
-          )}
-        </Box>
+        {/* Inbox */}
+        <Link href="/inbox">
+          <NotificationButton hasNew={hasNewNotifications} />
+        </Link>
+
+        {/* Account */}
+        {isLoading ? (
+          <Box width={40} height={40} />
+        ) : isLoggedIn ? (
+          <Link href="/my-page" underline="none">
+            <AccountButton />
+          </Link>
+        ) : (
+          <LinkButton
+            href="/sign-in"
+            buttonProps={{ sx: { fontWeight: 'medium' } }}
+          >
+            SIGN IN
+          </LinkButton>
+        )}
       </Container>
     </MaterialAppBar>
   );
